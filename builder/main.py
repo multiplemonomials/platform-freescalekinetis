68af1232325c87f1bb86b7e0e3a9663c67347352
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2025-09-06 17:06:30 -0700

    Remove newline

--- builder/main.py
+++ builder/main.py
@@ -96,7 +96,6 @@ else:
     target_elf = env.BuildProgram()
     target_firm = env.ElfToBin(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
     env.Depends(target_firm, "checkprogsize")
-   
 
 AlwaysBuild(env.Alias("nobuild", target_firm))
 target_buildprog = env.Alias("buildprog", target_firm, target_firm)
